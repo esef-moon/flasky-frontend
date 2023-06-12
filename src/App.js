b951c1072c51b2900a7f906290f6681c433d45cd
Susi <susi.franco32@gmail.com>
2023-06-12 14:55:08 -0700

    created Animal Component to display one cat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Animal from './components/Animal';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <section>
+    <h1> Sapphire Animals</h1>
+    <h2> Animal List</h2>
+  {/* Make a list of animals */}
+  <Animal></Animal>
+  <Animal></Animal>
+  <Animal></Animal>
+  <Animal></Animal>
+  {/*  */}
+  </section>
   );
 }
 
---
+++ src/components/Animal.js
@@ -0,0 +1,14 @@
+import './Animal.css';
+
+const Animal = () => {
+
+    return (
+        <section className="Animal">
+            <h3> Name: Willow</h3>
+            <p> nickname: Simon's bb cat </p>
+            <img src="https://placekitten.com/200" alt="Willow Simon's cat"></img>
+        </section>
+    );
+}
+
+export default Animal;
