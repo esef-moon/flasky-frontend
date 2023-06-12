6217582e6670dfe6fadb81aeb6cb77c2a79e0b7b
Susi <susi.franco32@gmail.com>
2023-06-12 14:59:42 -0700

    created animal list component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Animal from './components/Animal';
-
+import AnimalList from './components/AnimalList';
 
 function App() {
   return (
@@ -10,11 +10,13 @@ function App() {
     <h1> Sapphire Animals</h1>
     <h2> Animal List</h2>
   {/* Make a list of animals */}
+  <AnimalList></AnimalList>
+  {/* Display animal pic, name, and nickname */}
   <Animal></Animal>
   <Animal></Animal>
   <Animal></Animal>
   <Animal></Animal>
-  {/*  */}
+
   </section>
   );
 }
---
+++ src/components/AnimalList.js
@@ -0,0 +1,11 @@
+import './AnimalList.css'
+
+const AnimalList = () => {
+    return (
+        <section>
+            <h4>All our Pets</h4>
+        </section>
+    )
+}
+
+export default AnimalList;
